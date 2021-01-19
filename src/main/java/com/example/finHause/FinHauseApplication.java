0abae49a512bf7374dc1a417b42e85c3779245bb
P1Hk0 <36180022+P1Hk0@users.noreply.github.com>
2021-01-19 13:58:24 +0800

    first commit

---
+++ src/main/java/com/example/finHause/FinHauseApplication.java
@@ -0,0 +1,13 @@
+package com.example.finHause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinHauseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinHauseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finHause/FinHauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finHause;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinHauseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
